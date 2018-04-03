b47c0c39aeac015fbf102926e2a504f2c13e52f6
Sagi Gleizer <sagigleizer@gmail.com>
2018-04-03 23:15:10 +0300

    added new 1500 TP data samples and 2000 FP data samples better judgment function.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 'use strict';
 
-var objects = require('./examples.json');
+//var objects = require('./examples.json');
+var comdboData = require('./combo-data.js');
 var comboNames = require('./ComboBoxNames')[0].comboBoxNames;
+var nonComdboData = require('./non-combo-data.js');
 var isComboCount = 0;
+var isFalseComboCount = 0;
+
 
 /**
  @returns
@@ -19,29 +23,79 @@ function isCombobox(element){
         }
     }
 
-    if (!isCombo){
-        isCombo = isComboString(StringOuterHTML);
+    (comboNames.indexOf(element.tagName.toUpperCase()) > -1)
+
+    if (!isCombo && StringOuterHTML != null){
+        isCombo = isComboString(StringOuterHTML,"class=");
+        if(!isCombo){
+            isCombo = isComboString(StringOuterHTML,"role=");
+            if(!isCombo){
+              // isCombo = isComboString(StringOuterHTML,"id=");
+            }
+        }
     }
+
+    if (!isCombo && element.class != null){
+        // || element.binding.toLowerCase().includes("select") will improve alot the finding of TP but add no less FP
+        isCombo = element.class.toLowerCase().includes("combo");
+    }
+
+    if (!isCombo && element.binding != null){
+        // || element.binding.toLowerCase().includes("select") will improve alot the finding of TP but add no less FP
+        isCombo = element.binding.toLowerCase().includes("combo");
+    }
+
+    if (!isCombo && element.role != null){
+        // || element.binding.toLowerCase().includes("select") will improve alot the finding of TP but add no less FP
+        isCombo = element.role.toLowerCase().includes("combo");
+    }
+
     console.log((i + 1) + "." + isCombo);
     return isCombo;
 }
+function isComboString(str,att){
+    let isCombo = false;
 
-for (var i = 0; i < objects.length; i++){
-  if (isCombobox(objects[i].elementInfo.element)){
-     isComboCount++;
-  }
-}
-
-function isComboString(str){
-    let firstCut = str.substring(str.lastIndexOf("class="));
+    let firstCut = str.substring(str.indexOf(att));
     let firstQindex = firstCut.indexOf("\"",0);
-    let secondQindex =firstCut.indexOf("\"",10);
-    let secondCut = firstCut.substring(firstQindex,secondQindex + 1);
+    let secondQindex =firstCut.indexOf("\"",7);
+    let secondCut = firstCut.substring(firstQindex,secondQindex + 1).toLowerCase();
 
-    return secondCut.includes("select");
+    if ( secondCut.includes("select") ||  secondCut.includes("option") || secondCut.includes("k-input") || secondCut.includes("dropdown")) {
+        isCombo =(!secondCut.includes("selectbox"))
+    }
+    return isCombo;
 }
 
-console.log("DATA SAMPLES COUNTER: " + objects.length);
+
+for (var i = 0; i < comdboData.length; i++){
+    //debuging helper
+    if (i == 0){
+        console.log("stop");
+    }
+    if (isCombobox(comdboData[i].elementInfo.element)){
+        isComboCount++;
+    }
+}
+
+for (var i = 0; i < nonComdboData.length; i++){
+    //debuging helper
+    if (i == 0 ){
+        console.log("stop");
+    }
+    if (isCombobox(nonComdboData[i].elementInfo.element)){
+        isFalseComboCount++;
+    }
+}
+
+console.log("COMBOBOX DATA");
+console.log("DATA SAMPLES COUNTER: " + comdboData.length);
 console.log("POSITIVE DATA SAMPLES: " + isComboCount);
-console.log("POSITIVE PERCENTAGE: " + isComboCount/ objects.length * 100 +"%");
+console.log("POSITIVE PERCENTAGE: " + isComboCount/ comdboData.length * 100 +"%");
+
+console.log("\nNON-COMBOBOX DATA");
+console.log("DATA SAMPLES COUNTER: " + nonComdboData.length);
+console.log("POSITIVE DATA SAMPLES: " + isFalseComboCount);
+console.log("POSITIVE PERCENTAGE: " + isFalseComboCount/ nonComdboData.length * 100 +"%");
+
 
